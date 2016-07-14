737af48ff04d62a7d2ec0f16acd96d060de93f3c
c1ay <liukaiash@gmail.com>
2016-07-14 14:31:05 +0800

    hiho  1014

---
+++ 1014.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+ 
+struct Tire{
+     int num;     Tire* next[26];
+     Tire()
+     {
+         num=0;
+         int i;
+         for(i=0;i<26;i++)
+             next[i] = NULL;
+     }
+}tree;
+ 
+void Insert(char word[])
+{
+    Tire* p = &tree;
+    int i;
+    for(i=0;word[i];i++){
+         int t = word[i]-'a';
+        if(p->next[t]==NULL)
+             p->next[t] = new Tire;
+        p = p->next[t];
+        p->num++;
+     }
+ }
+ 
+ int Find(char word[])
+ {
+    Tire* p = &tree;
+     int i;
+     for(i=0;word[i];i++){
+        int t = word[i]-'a';
+        if(p->next[t]==NULL)
+             return 0;
+        p = p->next[t];
+     }
+     return p->num;
+ }
+ 
+ int main()
+ {
+     int n;
+    char word[11];
+     //字典
+     scanf("%d",&n);
+     gets(word);
+     while(n--){    //读取单词n次
+         gets(word);    
+         Insert(word);    //将单词插入到字典树中
+    }
+    //询问
+     scanf("%d",&n);
+     gets(word);    //将回车读入
+     while(n--){
+         gets(word);
+        printf("%d\n",Find(word));    //查询以word为前缀的单词出现过几次
+     }
+    return 0;
+}
---
+++ 1014.py
@@ -0,0 +1,41 @@
+def add_cond(word, cond):
+    if not word:
+        return
+    if word[0] in cond.children:
+        # if c.value == word[0]:
+        c = cond.children[word[0]]
+        c.count += 1
+        return add_cond(word[1:], c)
+    else:
+        c = Cond(word[0], 1)
+        cond.children[word[0]] = c
+        return add_cond(word[1:], c)
+
+def dfs(word, cond):
+    if not word:
+        return cond.count
+    if word[0] in cond.children:
+        # if cond.value == word[0]:
+        return dfs(word[1:], cond.children[word[0]])
+    else:
+        return 0
+class Cond:
+
+    def __init__(self, v, c=0):
+        self.value = v
+        self.count = c
+        self.children = {}
+
+def run():
+    c = input()
+    root = Cond('')
+    for _ in xrange(int(c)):
+        input_str = input()
+        add_cond(input_str, root)
+    c = input()
+    for _ in xrange(int(c)):
+        input_str = input()
+        print(dfs(input_str, root))
+
+if __name__ == '__main__':
+    run()
